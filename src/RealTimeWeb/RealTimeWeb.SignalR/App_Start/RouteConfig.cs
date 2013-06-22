51cbc2937b8d9479a41c8ce52b28ed42ab1cfdb9
tsubik <tsubik@gmail.com>
2013-06-22 23:14:04 +0200

    some refactoring

--- src/RealTimeWeb/RealTimeWeb.SignalR/App_Start/RouteConfig.cs
+++ src/RealTimeWeb/RealTimeWeb.SignalR/App_Start/RouteConfig.cs
@@ -12,6 +12,7 @@ namespace RealTimeWeb.SignalR
 		public static void RegisterRoutes(RouteCollection routes)
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+			routes.IgnoreRoute("");
 
 			routes.MapRoute(
 				name: "Default",
